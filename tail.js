80fdfb0a532d6dcd4c726997427a980b2eeb1f67
Courtney Amos <c.amos@shaw.ca>
2021-03-29 15:48:13 -0700

    tail function that returns a new array containing every element of the array except the first, without modifying the original array

---
+++ tail.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let newArr = [];
+
+  newArr = arr.slice(1);
+  return newArr;
+};
+
+// test cases
+const result = tail(["Yo Yo", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const test = ["Yo Yo"];
+tail(test);
+assertEqual(test.length, 1);
