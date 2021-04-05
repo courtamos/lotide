343c73d9413d052a23ff81bbc84537e0c73b9a30
Courtney Amos <c.amos@shaw.ca>
2021-04-05 13:16:15 -0700

    set lotide project up with mocha and chai, and converted headTest to use them instead

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,12 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5); // pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // pass
-assertEqual(head([1]), 1); // pass
-assertEqual(head([]), 5); // fail, undefined
-assertEqual(head([5,6,7]), 6); // fail
-assertEqual(head([]),); // pass, undefined
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
